58f6d5d7b05883786839717aeae20e3128d52dcf
Piyush Sharma <2002piyush6@gmail.com>
2024-07-24 12:54:27 +0530

    Update lazy loading

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -17,6 +17,7 @@ const Card = ({_id, name, prompt, photo}) => {
   return (
     <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">
       <img 
+        loading='lazy'
         className='w-full h-auto object-cover rounded-xl'
         src={photo}
         alt={prompt}
